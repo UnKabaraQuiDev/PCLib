029b0465ec69137f860f0e8dabe24863b320f88f
P.Cy.113 <poucy113@github.com>
2024-04-07 14:06:00 +0200

    Removed useless imports in PCLogger

--- src/lu/pcy113/pclib/PCLogger.java
+++ src/lu/pcy113/pclib/PCLogger.java
@@ -11,7 +11,6 @@ import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 import java.util.logging.Level;
