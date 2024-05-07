ee696f394bcbddfd7801838fbb2bae2388d31f8b
UnKabaraQuiDev <unkabaraquidev@github.com>
2024-05-07 21:40:29 +0200

    Added ThreadBuilder

---
+++ src/lu/pcy113/pclib/ThreadBuilder.java
@@ -0,0 +1,42 @@
+package lu.pcy113.pclib;
+
+import java.lang.Thread.UncaughtExceptionHandler;
+
+public class ThreadBuilder {
+	
+	private Thread thread;
+	
+	private ThreadBuilder(Runnable run) {
+		thread = new Thread(run);
+	}
+	
+	public ThreadBuilder daemon(boolean daemon) {
+		thread.setDaemon(daemon);
+		return this;
+	}
+	
+	public ThreadBuilder name(String name) {
+		thread.setName(name);
+		return this;
+	}
+	
+	public ThreadBuilder priority(int priority) {
+		thread.setPriority(priority);
+		return this;
+	}
+	
+	public ThreadBuilder except(UncaughtExceptionHandler eh) {
+		thread.setUncaughtExceptionHandler(eh);
+		return this;
+	}
+	
+	public Thread start() {
+		thread.start();
+		return thread;
+	}
+	
+	public static ThreadBuilder create(Runnable run) {
+		return new ThreadBuilder(run);
+	}
+	
+}
