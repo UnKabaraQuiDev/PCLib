475cae539c8f9767aba5f38175fca416c8a515e0
UnKabaraQuiDev <unkabaraquidev@github.com>
2024-05-12 16:09:03 +0200

    Added util functions to IntPointer

--- src/lu/pcy113/pclib/pointer/prim/IntPointer.java
+++ src/lu/pcy113/pclib/pointer/prim/IntPointer.java
@@ -2,21 +2,58 @@ package lu.pcy113.pclib.pointer.prim;
 
 import lu.pcy113.pclib.pointer.ObjectPointer;
 
-public class IntPointer
-		extends
-		PrimitivePointer<Integer> {
+public class IntPointer extends PrimitivePointer<Integer> {
 
 	private int value;
 
-	public IntPointer() {}
+	public IntPointer() {
+		this.value = 0;
+	}
 
 	public IntPointer(int value) {
 		this.value = value;
 	}
 
-	public int getValue() { return this.value; }
+	public int getValue() {
+		return this.value;
+	}
 
-	public void setValue(int value) { this.value = value; }
+	public synchronized void setValue(int value) {
+		this.value = value;
+	}
+
+	public synchronized int increment() {
+		return ++this.value;
+	}
+
+	public synchronized int decrement() {
+		return ++this.value;
+	}
+
+	public synchronized int add(int other) {
+		this.value += other;
+		return value;
+	}
+
+	public synchronized int mul(int other) {
+		this.value *= other;
+		return value;
+	}
+
+	public synchronized int sub(int other) {
+		this.value -= other;
+		return value;
+	}
+
+	public synchronized int div(int other) {
+		this.value /= other;
+		return value;
+	}
+
+	public synchronized int mod(int other) {
+		this.value %= other;
+		return value;
+	}
 
 	@Override
 	public ObjectPointer<Integer> toObjectPointer() {
