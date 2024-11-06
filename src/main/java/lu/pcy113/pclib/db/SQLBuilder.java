a1644dffe3a681eec17fc550d36e5d1efae1ddb4
UnKabaraQuiDev <poucy113@school.lu>
2024-11-06 15:08:57 +0100

    Updated SQLBuilder#safeUpdate added WHERE clause

--- src/main/java/lu/pcy113/pclib/db/SQLBuilder.java
+++ src/main/java/lu/pcy113/pclib/db/SQLBuilder.java
@@ -9,12 +9,12 @@ import lu.pcy113.pclib.db.impl.SQLEntry;
 public class SQLBuilder {
 
 	public static <T extends SQLEntry> String safeInsert(DataBaseTable<T> table, String[] columns) {
-		return "INSERT INTO `" + table.getTableName() + "` (" + Arrays.stream(columns).map(i -> "`" + i + "`").collect(Collectors.joining(", ")) + ") VALUES (" + Arrays.stream(columns).map((i) -> "?").collect(Collectors.joining(", "))
-				+ ");";
+		return "INSERT INTO `" + table.getTableName() + "` (" + Arrays.stream(columns).map(i -> "`" + i + "`").collect(Collectors.joining(", ")) + ") VALUES (" + Arrays.stream(columns).map((i) -> "?").collect(Collectors.joining(", ")) + ");";
 	}
 
-	public static <T extends SQLEntry> String safeUpdate(DataBaseTable<T> table, String[] columns) {
-		return "UPDATE `" + table.getTableName() + "` SET " + Arrays.stream(columns).map(i -> "`" + i + "` = ?").collect(Collectors.joining(", ")) + ";";
+	public static <T extends SQLEntry> String safeUpdate(DataBaseTable<T> table, String[] columns, String[] whereColumns) {
+		return "UPDATE `" + table.getTableName() + "` SET " + Arrays.stream(columns).map(i -> "`" + i + "` = ?").collect(Collectors.joining(", ")) + " WHERE "
+				+ Arrays.stream(whereColumns).map(i -> "`" + i + "` = ?").collect(Collectors.joining(" AND ")) + ";";
 	}
 
 	public static <T extends SQLEntry> String safeDelete(DataBaseTable<T> table, String[] columns) {
@@ -30,7 +30,7 @@ public class SQLBuilder {
 	}
 
 	public static <T extends SQLEntry> String count(DataBaseTable<T> table) {
-		return "SELECT count(*) as `count` FROM `"+table.getTableName()+"`;";
+		return "SELECT count(*) as `count` FROM `" + table.getTableName() + "`;";
 	}
 
 }
