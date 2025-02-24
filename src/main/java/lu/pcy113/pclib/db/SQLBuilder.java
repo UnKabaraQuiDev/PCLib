ab662daec460d25020726991951cdd92577d65bf
UnKabaraQuiDev <66266372+UnKabaraQuiDev@users.noreply.github.com>
2025-02-24 10:28:47 +0100

    Fixed wrong method signature in SQLBuilder

--- src/main/java/lu/pcy113/pclib/db/SQLBuilder.java
+++ src/main/java/lu/pcy113/pclib/db/SQLBuilder.java
@@ -10,28 +10,29 @@ import lu.pcy113.pclib.db.impl.SQLEntry;
 public class SQLBuilder {
 
 	public static int ENTRY_LIMIT = 500;
-	
+
 	public static <T extends SQLEntry> String safeInsert(DataBaseTable<T> table, String[] columns) {
-		return "INSERT INTO `" + table.getTableName() + "` (" + Arrays.stream(columns).filter(Objects::nonNull).map(i -> "`" + i + "`").collect(Collectors.joining(", ")) + ") VALUES (" + Arrays.stream(columns).map((i) -> "?").collect(Collectors.joining(", ")) + ");";
+		return "INSERT INTO `" + table.getTableName() + "` (" + Arrays.stream(columns).filter(Objects::nonNull).map(i -> "`" + i + "`").collect(Collectors.joining(", ")) + ") VALUES ("
+				+ Arrays.stream(columns).map((i) -> "?").collect(Collectors.joining(", ")) + ");";
 	}
 
 	public static <T extends SQLEntry> String safeUpdate(DataBaseTable<T> table, String[] columns, String[] whereColumns) {
 		return "UPDATE `" + table.getTableName() + "` SET " + Arrays.stream(columns).filter(Objects::nonNull).map(i -> "`" + i + "` = ?").collect(Collectors.joining(", ")) + " WHERE "
-				+ Arrays.stream(whereColumns).filter(Object::nonNull).map(i -> "`" + i + "` = ?").collect(Collectors.joining(" AND ")) + ";";
+				+ Arrays.stream(whereColumns).filter(Objects::nonNull).map(i -> "`" + i + "` = ?").collect(Collectors.joining(" AND ")) + ";";
 	}
 
 	public static <T extends SQLEntry> String safeDelete(DataBaseTable<T> table, String[] columns) {
 		return "DELETE FROM `" + table.getTableName() + "` WHERE " + Arrays.stream(columns).filter(Objects::nonNull).map(i -> "`" + i + "` = ?").collect(Collectors.joining(" AND ")) + ";";
 	}
 
-	public static  <T extends SQLEntry> String safeSelect(DataBaseTable<T> table, String[] whereColumns) {
+	public static <T extends SQLEntry> String safeSelect(DataBaseTable<T> table, String[] whereColumns) {
 		return safeSelect(table, whereColumns, -1);
 	}
-	
-	public static  <T extends SQLEntry> String safeSelect(DataBaseTable<T> table, String[] whereColumns, int limit) {
-		return "SELECT * FROM `" + table.getTableName() + "`" + (whereColumns == null || whereColumns.length == 0 ? "" : " WHERE " + Arrays.stream(whereColumns).filter(Objects::nonNull).map(i -> "`" + i + "` = ?").collect(Collectors.joining(" AND ")))
-														+ (limit > -1 ? " LIMIT " + limit : " LIMIT "+ENTRY_LIMIT)
-														+ ";";
+
+	public static <T extends SQLEntry> String safeSelect(DataBaseTable<T> table, String[] whereColumns, int limit) {
+		return "SELECT * FROM `" + table.getTableName() + "`"
+				+ (whereColumns == null || whereColumns.length == 0 ? "" : " WHERE " + Arrays.stream(whereColumns).filter(Objects::nonNull).map(i -> "`" + i + "` = ?").collect(Collectors.joining(" AND ")))
+				+ (limit > -1 ? " LIMIT " + limit : " LIMIT " + ENTRY_LIMIT) + ";";
 	}
 
 	public static <T extends SQLEntry> String safeSelectUniqueCollision(DataBaseTable<T> table, Stream<String> whereColumns) {
