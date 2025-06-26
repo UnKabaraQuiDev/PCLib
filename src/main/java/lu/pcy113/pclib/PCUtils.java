a3e504dd7523163382b3456698fa2acc749da7e5
UnKabaraQuiDev <66266372+UnKabaraQuiDev@users.noreply.github.com>
2025-06-26 16:26:32 +0200

    added getAllFields in PCUTils)

--- src/main/java/lu/pcy113/pclib/PCUtils.java
+++ src/main/java/lu/pcy113/pclib/PCUtils.java
@@ -11,6 +11,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.lang.reflect.Array;
+import java.lang.reflect.Field;
 import java.lang.reflect.GenericArrayType;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
@@ -1432,6 +1433,16 @@ public final class PCUtils {
 		}
 
 		return result.toString();
+	}
+
+	public static List<Field> getAllFields(Class<?> clazz) {
+		List<Field> fields = new ArrayList<>();
+		Class<?> current = clazz;
+		while (current != null && current != Object.class) {
+			Collections.addAll(fields, current.getDeclaredFields());
+			current = current.getSuperclass();
+		}
+		return fields;
 	}
 
 }
