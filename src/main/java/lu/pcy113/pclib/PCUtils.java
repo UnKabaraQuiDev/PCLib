de4c704f0d35d68151ab8da5ff3a70e24d32e53f
UnKabaraQuiDev <66266372+UnKabaraQuiDev@users.noreply.github.com>
2025-10-07 21:32:24 +0200

    updated PCUtils

--- src/main/java/lu/pcy113/pclib/PCUtils.java
+++ src/main/java/lu/pcy113/pclib/PCUtils.java
@@ -236,12 +236,12 @@ public final class PCUtils {
 			StackTraceElement ste = stElements[i];
 			if (!PCUtils.class.getName().equals(ste.getClassName())) {
 				if (!parent) {
-					return (simple ? PCUtils.getFileExtension(ste.getClassName()) : ste.getClassName()) + "#" + ste.getMethodName() + "@"
-							+ ste.getLineNumber();
+					return (simple ? PCUtils.getFileExtension(ste.getClassName()) : ste.getClassName()) + "#"
+							+ ste.getMethodName() + "@" + ste.getLineNumber();
 				} else {
 					ste = stElements[i + 1];
-					return (simple ? PCUtils.getFileExtension(ste.getClassName()) : ste.getClassName()) + "#" + ste.getMethodName() + "@"
-							+ ste.getLineNumber();
+					return (simple ? PCUtils.getFileExtension(ste.getClassName()) : ste.getClassName()) + "#"
+							+ ste.getMethodName() + "@" + ste.getLineNumber();
 				}
 
 			}
@@ -265,8 +265,8 @@ public final class PCUtils {
 				} else if (i + 1 < stElements.length) {
 					StackTraceElement parentSte = stElements[i + 1];
 					String parentClassName = parentSte.getClassName();
-					return (simple ? PCUtils.getFileExtension(parentClassName) : parentClassName) + "#" + parentSte.getMethodName() + "@"
-							+ parentSte.getLineNumber();
+					return (simple ? PCUtils.getFileExtension(parentClassName) : parentClassName) + "#"
+							+ parentSte.getMethodName() + "@" + parentSte.getLineNumber();
 				}
 			}
 		}
@@ -282,7 +282,8 @@ public final class PCUtils {
 			StackTraceElement ste = stElements[i];
 			String className = ste.getClassName();
 
-			if (!PCUtils.class.getName().equals(className) && regexList.stream().noneMatch(p -> p.matcher(className).matches())) {
+			if (!PCUtils.class.getName().equals(className)
+					&& regexList.stream().noneMatch(p -> p.matcher(className).matches())) {
 				if (!parent) {
 					return (simple ? PCUtils.getFileExtension(className) : className) + "#" + ste.getMethodName() + "@"
 							+ ste.getLineNumber();
@@ -290,8 +291,8 @@ public final class PCUtils {
 					StackTraceElement parentSte = stElements[i + 1];
 					String parentClassName = parentSte.getClassName();
 
-					return (simple ? PCUtils.getFileExtension(parentClassName) : parentClassName) + "#" + parentSte.getMethodName() + "@"
-							+ parentSte.getLineNumber();
+					return (simple ? PCUtils.getFileExtension(parentClassName) : parentClassName) + "#"
+							+ parentSte.getMethodName() + "@" + parentSte.getLineNumber();
 				}
 			}
 		}
@@ -310,8 +311,8 @@ public final class PCUtils {
 			StackTraceElement ste = stElements[i];
 			if (!PCUtils.class.getName().equals(ste.getClassName())) {
 				ste = stElements[i + offset];
-				return (simple ? PCUtils.getFileExtension(ste.getClassName()) : ste.getClassName()) + "#" + ste.getMethodName() + "@"
-						+ ste.getLineNumber();
+				return (simple ? PCUtils.getFileExtension(ste.getClassName()) : ste.getClassName()) + "#"
+						+ ste.getMethodName() + "@" + ste.getLineNumber();
 			}
 		}
 		return null;
@@ -331,7 +332,8 @@ public final class PCUtils {
 	}
 
 	public static byte[] intToByteArray(int val) {
-		return new byte[] { (byte) ((val >> 24) & 0xFF), (byte) ((val >> 16) & 0xFF), (byte) ((val >> 8) & 0xFF), (byte) (val & 0xFF) };
+		return new byte[] { (byte) ((val >> 24) & 0xFF), (byte) ((val >> 16) & 0xFF), (byte) ((val >> 8) & 0xFF),
+				(byte) (val & 0xFF) };
 	}
 
 	public static byte[] remainingByteBufferToArray(ByteBuffer bb) {
@@ -773,10 +775,11 @@ public final class PCUtils {
 		}
 	}
 
-	private static final Collector<?, ?, ?> SHUFFLER = Collectors.collectingAndThen(Collectors.toCollection(ArrayList::new), list -> {
-		Collections.shuffle(list);
-		return list;
-	});
+	private static final Collector<?, ?, ?> SHUFFLER = Collectors
+			.collectingAndThen(Collectors.toCollection(ArrayList::new), list -> {
+				Collections.shuffle(list);
+				return list;
+			});
 
 	@SuppressWarnings("unchecked")
 	public static <T> Collector<T, ?, List<T>> toShuffledList() {
@@ -863,7 +866,8 @@ public final class PCUtils {
 	public static Set<Class<?>> getTypesInPackage(String packageName) {
 		InputStream stream = ClassLoader.getSystemClassLoader().getResourceAsStream(packageName.replaceAll("[.]", "/"));
 		BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-		return reader.lines().filter(line -> line.endsWith(".class")).map(line -> getClass(line, packageName)).collect(Collectors.toSet());
+		return reader.lines().filter(line -> line.endsWith(".class")).map(line -> getClass(line, packageName))
+				.collect(Collectors.toSet());
 	}
 
 	private static Class<?> getClass(String className, String packageName) {
@@ -914,7 +918,8 @@ public final class PCUtils {
 
 	public static List<String> recursiveList(Path directory) throws IOException {
 		try (Stream<Path> walk = Files.walk(directory)) {
-			return walk.filter(Files::isRegularFile).map(path -> directory.relativize(path).toString()).collect(Collectors.toList());
+			return walk.filter(Files::isRegularFile).map(path -> directory.relativize(path).toString())
+					.collect(Collectors.toList());
 		}
 	}
 
@@ -996,7 +1001,8 @@ public final class PCUtils {
 	 * "abc", " ", 5 -> " abc"
 	 */
 	public static String leftPadStringLeftTrim(String str, String fill, int length) {
-		return (str.length() < length ? repeatString(fill, length - str.length()) + str : leftTrimToLength(str, length));
+		return (str.length() < length ? repeatString(fill, length - str.length()) + str
+				: leftTrimToLength(str, length));
 	}
 
 	/**
@@ -1004,7 +1010,8 @@ public final class PCUtils {
 	 * "abc", " ", 5 -> " abc"
 	 */
 	public static String leftPadStringRightTrim(String str, String fill, int length) {
-		return (str.length() < length ? repeatString(fill, length - str.length()) + str : rightTrimToLength(str, length));
+		return (str.length() < length ? repeatString(fill, length - str.length()) + str
+				: rightTrimToLength(str, length));
 	}
 
 	public static ByteBuffer readFile(File file) throws IOException {
@@ -1036,8 +1043,8 @@ public final class PCUtils {
 
 	@DependsOn("lu.pcy113.jbcodec")
 	/*
-	 * public static Object decodeFile(CodecManager cm, File file) throws IOException { return
-	 * cm.decode(readFile(file)); }
+	 * public static Object decodeFile(CodecManager cm, File file) throws
+	 * IOException { return cm.decode(readFile(file)); }
 	 */
 
 	public static String leftPadLine(String str, String fill) {
@@ -1070,8 +1077,8 @@ public final class PCUtils {
 	}
 
 	/**
-	 * Extracts all keys from the given JSONObject, including nested keys, in the format of
-	 * "key.subkey".
+	 * Extracts all keys from the given JSONObject, including nested keys, in the
+	 * format of "key.subkey".
 	 *
 	 * @param jsonObject The JSONObject to extract keys from.
 	 * @return A Set containing all keys in the desired format.
@@ -1120,18 +1127,18 @@ public final class PCUtils {
 			}
 
 			configFile.createNewFile();
-			Files.copy(clazz.getResourceAsStream(inPath), Paths.get(configFile.getPath()), StandardCopyOption.REPLACE_EXISTING);
+			Files.copy(clazz.getResourceAsStream(inPath), Paths.get(configFile.getPath()),
+					StandardCopyOption.REPLACE_EXISTING);
 
 			return true;
 		}
 		return false;
 	}
 
-	public static <K, V> Map<K, V> castMap(Map<?, ?> map, Supplier<Map<K, V>> supplier, Class<K> keyClass, Class<V> valueClass) {
-		return map
-				.entrySet()
-				.stream()
-				.collect(Collectors.toMap(e -> keyClass.cast(e.getKey()), e -> valueClass.cast(e.getValue()), (k1, k2) -> k1, supplier));
+	public static <K, V> Map<K, V> castMap(Map<?, ?> map, Supplier<Map<K, V>> supplier, Class<K> keyClass,
+			Class<V> valueClass) {
+		return map.entrySet().stream().collect(Collectors.toMap(e -> keyClass.cast(e.getKey()),
+				e -> valueClass.cast(e.getValue()), (k1, k2) -> k1, supplier));
 	}
 
 	public static <T> T throw_(Throwable e) throws Throwable {
@@ -1183,11 +1190,13 @@ public final class PCUtils {
 	}
 
 	/*
-	 * public static short map(short x, short in_min, short in_max, short out_min, short out_max) {
-	 * return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min; }
+	 * public static short map(short x, short in_min, short in_max, short out_min,
+	 * short out_max) { return (x - in_min) * (out_max - out_min) / (in_max -
+	 * in_min) + out_min; }
 	 * 
-	 * public static byte map(byte x, byte in_min, byte in_max, byte out_min, byte out_max) { return (x
-	 * - in_min) * (out_max - out_min) / (in_max - in_min) + out_min; }
+	 * public static byte map(byte x, byte in_min, byte in_max, byte out_min, byte
+	 * out_max) { return (x - in_min) * (out_max - out_min) / (in_max - in_min) +
+	 * out_min; }
 	 */
 
 	/**
@@ -1312,7 +1321,8 @@ public final class PCUtils {
 				// Insert underscore if:
 				// - The previous char is lowercase -> start of a new word
 				// - the next char is lowercase -> end of a acronym
-				if (i > 0 && (Character.isLowerCase(chars[i - 1]) || (i + 1 < chars.length && Character.isLowerCase(chars[i + 1])))) {
+				if (i > 0 && (Character.isLowerCase(chars[i - 1])
+						|| (i + 1 < chars.length && Character.isLowerCase(chars[i + 1])))) {
 					result.append('_');
 				}
 				result.append(Character.toLowerCase(current));
@@ -1568,10 +1578,8 @@ public final class PCUtils {
 	}
 
 	public static Color maxContrast(Color background, Stream<Color> stream) {
-		return stream
-				.sorted((c1, c2) -> (int) Math.signum(contrast(background, c2) - contrast(background, c1)))
-				.findFirst()
-				.orElseGet(() -> maxContrast(background));
+		return stream.sorted((c1, c2) -> (int) Math.signum(contrast(background, c2) - contrast(background, c1)))
+				.findFirst().orElseGet(() -> maxContrast(background));
 	}
 
 	public static void close(Closeable... result) throws IOException {
@@ -1638,7 +1646,8 @@ public final class PCUtils {
 		}
 	}
 
-	public static <T> Constructor<T> findCompatibleConstructor(Class<T> clazz, Class<?>... argTypes) throws NoSuchMethodException {
+	public static <T> Constructor<T> findCompatibleConstructor(Class<T> clazz, Class<?>... argTypes)
+			throws NoSuchMethodException {
 
 		for (Constructor<?> ctor : clazz.getConstructors()) {
 			Class<?>[] params = ctor.getParameterTypes();
@@ -1659,7 +1668,8 @@ public final class PCUtils {
 			}
 		}
 
-		throw new NoSuchMethodException("No compatible constructor found in " + clazz.getName());
+		throw new NoSuchMethodException(
+				"No compatible constructor found in " + clazz.getName() + " for args: " + Arrays.toString(argTypes));
 	}
 
 	public static String progressBar(long budgetNanos, long usedNanos, boolean showOverFlow) {
