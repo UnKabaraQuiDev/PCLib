58052c45d3a65e439f1d2a5e82c7778056386ee2
UnKabaraQuiDev <66266372+UnKabaraQuiDev@users.noreply.github.com>
2026-02-06 19:26:35 +0100

    Moving to submodules

---
+++ pclib-datastruct/src/main/java/lu/pcy113/pclib/datastructure/pair/ReadOnlyPair.java
@@ -0,0 +1,35 @@
+package lu.pcy113.pclib.datastructure.pair;
+
+public class ReadOnlyPair<K, V> extends Pair<K, V> {
+
+	public ReadOnlyPair() {
+		super();
+	}
+
+	public ReadOnlyPair(K k, V v) {
+		super(k, v);
+	}
+
+	@Override
+	@Deprecated
+	public ReadOnlyPair<K, V> setKey(K key) {
+		throw new UnsupportedOperationException("Operation not permitted on readonly pair !");
+	}
+
+	@Override
+	@Deprecated
+	public ReadOnlyPair<K, V> setValue(V value) {
+		throw new UnsupportedOperationException("Operation not permitted on readonly pair !");
+	}
+
+	@Override
+	public ReadOnlyPair<K, V> clone() {
+		return (ReadOnlyPair<K, V>) super.clone();
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s(readonly)", super.toString());
+	}
+
+}
