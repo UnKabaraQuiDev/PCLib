4a33ef4044d4d89c888f7cb9d9bcbea5a240d3bd
UnKabaraQuiDev <66266372+UnKabaraQuiDev@users.noreply.github.com>
2026-03-03 17:45:03 +0100

    tried to fix autoconfig for web mvc

--- pclib-db-spring/src/main/java/lu/kbra/pclib/db/config/PCLibDBAutoConfiguration.java
+++ pclib-db-spring/src/main/java/lu/kbra/pclib/db/config/PCLibDBAutoConfiguration.java
@@ -2,7 +2,6 @@ package lu.kbra.pclib.db.config;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.context.ConfigurationPropertiesAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
@@ -13,22 +12,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lu.kbra.pclib.db.registrar.DeferredSQLQueryableRegistrar;
 import lu.kbra.pclib.db.utils.SpringDataBaseEntryUtils;
 
-@AutoConfiguration(after = ConfigurationPropertiesAutoConfiguration.class)
+@AutoConfiguration
 public class PCLibDBAutoConfiguration {
 
 	@Bean
-	@Primary
 	@ConditionalOnMissingBean
-	public SpringDataBaseEntryUtils defaultSpringDataBaseEntryUtils(final ObjectMapper objectMapper,
+	public SpringDataBaseEntryUtils defaultSpringDataBaseEntryUtils(
+			final ObjectMapper objectMapper,
 			final ConversionService conversionService) {
 		return new SpringDataBaseEntryUtils(objectMapper, conversionService);
 	}
 
 	@Bean
-	@Primary
 	@ConditionalOnMissingBean
 	public DeferredSQLQueryableRegistrar defaultDeferredSQLQueryableRegistrar(
-			final ApplicationContext applicationContext, final SpringDataBaseEntryUtils dataBaseEntryUtils) {
+			final ApplicationContext applicationContext,
+			final SpringDataBaseEntryUtils dataBaseEntryUtils) {
 		return new DeferredSQLQueryableRegistrar(applicationContext, dataBaseEntryUtils);
 	}
 
