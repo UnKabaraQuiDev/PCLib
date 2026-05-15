4f3b433baca338fc07ab1e3d7140f6dc9dbb223c
UnKabaraQuiDev <u.kbra.lu@gmail.com>
2026-05-15 14:17:44 +0200

    fix: #13

--- pclib-db/src/main/java/lu/kbra/pclib/db/utils/BaseProxyDataBaseEntryUtils.java
+++ pclib-db/src/main/java/lu/kbra/pclib/db/utils/BaseProxyDataBaseEntryUtils.java
@@ -1,8 +1,6 @@
 package lu.kbra.pclib.db.utils;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.Arrays;
@@ -19,70 +17,13 @@ import lu.kbra.pclib.datastructure.tuple.Tuple;
 import lu.kbra.pclib.db.autobuild.column.type.mysql.ColumnType;
 import lu.kbra.pclib.db.autobuild.query.Query;
 import lu.kbra.pclib.db.impl.DataBaseEntry;
+import lu.kbra.pclib.db.impl.NTSQLQueryable;
 import lu.kbra.pclib.db.impl.SQLQueryable;
 import lu.kbra.pclib.db.utils.SimpleTransformingQuery.ListSimpleTransformingQuery;
 import lu.kbra.pclib.db.utils.SimpleTransformingQuery.MapSimpleTransformingQuery;
 import lu.kbra.pclib.impl.ThrowingFunction;
 
 public class BaseProxyDataBaseEntryUtils extends BaseDataBaseEntryUtils implements ProxyDataBaseEntryUtils {
-
-	@Override
-	public <T extends DataBaseEntry> void initQueries(final SQLQueryable<T> instance) {
-		final Class<? extends SQLQueryable<T>> tableClazz = instance.getTargetClass();
-		final Field[] tableFields = tableClazz.getDeclaredFields();
-		final String tableName = this.getQueryableName(tableClazz);
-
-		// scan the table itself
-		for (final Field field : tableFields) {
-			if (Modifier.isStatic(field.getModifiers()) || !field.isAnnotationPresent(Query.class)) {
-				continue;
-			}
-
-			field.setAccessible(true);
-
-			final Type fieldType = field.getGenericType();
-			if (!(fieldType instanceof ParameterizedType)) {
-				throw new IllegalArgumentException("Invalid query type: " + fieldType.getTypeName() + " for: " + field);
-			}
-
-			try {
-				final Object value = this
-						.buildTableQueryFunction(tableClazz, tableName, instance, fieldType, field.getAnnotation(Query.class));
-
-				if (field != null) {
-					field.set(instance, value);
-				}
-			} catch (final Exception e) {
-				throw new RuntimeException("Failed to initialize @Query field: " + field.getName() + ", from: " + tableClazz.getName(), e);
-			}
-		}
-	}
-
-	@Override
-	public <T extends DataBaseEntry> Object buildTableQueryFunction(
-			final Class<? extends SQLQueryable<T>> tableClazz,
-			final String tableName,
-			final SQLQueryable<T> instance,
-			final Type type,
-			final Query query) {
-		final String queryText = query.value().replace(Query.TABLE_NAME, PCUtils.sqlEscapeIdentifier(tableName));
-
-		if (query.limit() > query.offset() && !(query.offset() == -1 || query.limit() == -1)) {
-			throw new IllegalArgumentException("Invalid order: (offset) -> " + query.offset() + " (limit) -> " + query.limit()
-					+ ", should be in this order: <others> <limit> <offset>");
-		}
-
-		final ParameterizedType pt = (ParameterizedType) type;
-		final Type raw = pt.getRawType();
-
-		if (!NextTask.class.equals(raw)) {
-			throw new IllegalArgumentException("Unsupported query field type: " + raw);
-		}
-
-		return null;
-		// throw new UnsupportedOperationException("Assigning a NextTask in tables isn't
-		// supported yet.");
-	}
 
 	private Query.Type detectDefaultTableStrategy(final ParameterizedType pt) {
 		if (this.isListType(pt.getActualTypeArguments()[1])) {
@@ -126,8 +67,8 @@ public class BaseProxyDataBaseEntryUtils extends BaseDataBaseEntryUtils implemen
 			}
 
 			final Type returnType = method.getGenericReturnType();
-			if (returnType instanceof ParameterizedType && NextTask.class.equals(((ParameterizedType) returnType).getRawType())) { // for
-																																	// NextTask
+			if (returnType instanceof ParameterizedType && NextTask.class.equals(((ParameterizedType) returnType).getRawType())) {
+				// for NextTask
 
 				final ParameterizedType ptReturnType = (ParameterizedType) returnType;
 
@@ -137,15 +78,18 @@ public class BaseProxyDataBaseEntryUtils extends BaseDataBaseEntryUtils implemen
 					final String sql = SQLBuilder.safeSelect(tableName, cols, query.limit() != -1, query.offset() != -1);
 
 					return this.getSupplierForMethod(ptReturnType, instance, cols, sql, query);
-				} else { // for manual queries (with sql)
+				} else {
+					// for manual queries (with sql)
 					return this.getSupplierForMethod(ptReturnType, instance, queryText, query);
 				}
 
-			} else { // for direct return
+			} else {
+				// for direct return
 
 				final Type[] argTypes = method.getGenericParameterTypes();
 
-				if (queryText == null || queryText.isEmpty()) {// for automatic queries (by column only)
+				if (queryText == null || queryText.isEmpty()) {
+					// for automatic queries (by column only)
 					final String[] cols = query.columns();
 
 					final String sql = SQLBuilder.safeSelect(tableName, cols, query.limit() != -1, query.offset() != -1);
@@ -163,8 +107,12 @@ public class BaseProxyDataBaseEntryUtils extends BaseDataBaseEntryUtils implemen
 		}
 	}
 
-	/** by manual sql */
-	private <T extends DataBaseEntry> Function<List<Object>, ?> getSupplierForMethod(
+	/**
+	 * by manual sql
+	 * 
+	 * @deprecated {@link NTSQLQueryable} is deprecated
+	 */
+	private <T extends DataBaseEntry> Function<List<Object>, NextTask<?, ?, ?>> getSupplierForMethod(
 			final ParameterizedType pt,
 			final SQLQueryable<T> instance,
 			final String sql,
@@ -210,8 +158,13 @@ public class BaseProxyDataBaseEntryUtils extends BaseDataBaseEntryUtils implemen
 				type)));
 	}
 
-	/** by columns (automatic) */
-	private <T extends DataBaseEntry> Function<List<Object>, ?> getSupplierForMethod(
+	/**
+	 * by columns (automatic)
+	 * 
+	 * @deprecated {@link NTSQLQueryable} is deprecated
+	 */
+	@Deprecated
+	private <T extends DataBaseEntry> Function<List<Object>, NextTask<?, ?, ?>> getSupplierForMethod(
 			final ParameterizedType ptReturnType,
 			final SQLQueryable<T> instance,
 			String[] cols,
@@ -309,7 +262,6 @@ public class BaseProxyDataBaseEntryUtils extends BaseDataBaseEntryUtils implemen
 			return this.detectDefaultMethodStrategy(((ParameterizedType) returnType).getActualTypeArguments()[1]);
 		}
 		return Query.Type.FIRST_NULL;
-		// throw new IllegalArgumentException("Unsupported return type: " + returnType);
 	}
 
 	public Query.Type detectDefaultEntryStrategy(final ParameterizedType returnType) {
