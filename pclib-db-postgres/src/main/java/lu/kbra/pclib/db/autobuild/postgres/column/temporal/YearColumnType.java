503c60124b7bfd39f0bfa4ede36e42f1d41db07b
hmm-click-click <300442971+hmm-click-click@users.noreply.github.com>
2026-07-22 11:50:11 +0200

    fix: sqlite+postgres YEAR type

---
+++ pclib-db-postgres/src/main/java/lu/kbra/pclib/db/autobuild/postgres/column/temporal/YearColumnType.java
@@ -0,0 +1,31 @@
+package lu.kbra.pclib.db.autobuild.postgres.column.temporal;
+
+import java.lang.reflect.Type;
+import java.time.Year;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lu.kbra.pclib.db.autobuild.postgres.encoding.integer.IntEncodingType;
+import lu.kbra.pclib.db.domain.column.type.ColumnType;
+import lu.kbra.pclib.db.domain.column.type.EncodingType;
+import lu.kbra.pclib.db.utils.registry.EncodingTypeRegistry;
+
+@Getter
+@RequiredArgsConstructor
+public class YearColumnType implements ColumnType<Year, Integer> {
+
+	private final EncodingType<Integer> encodingType = EncodingTypeRegistry
+			.getFixedEncodingType(IntEncodingType.class, true, IntEncodingType::new);
+
+	@Override
+	public @NonNull Year decode(@NonNull Integer value, Type type) {
+		return Year.of(value);
+	}
+
+	@Override
+	public @NonNull Integer encode(@NonNull Year value) {
+		return value.getValue();
+	}
+
+}
---
+++ pclib-db-sqlite/src/main/java/lu/kbra/pclib/db/autobuild/sqlite/column/temporal/YearColumnType.java
@@ -0,0 +1,30 @@
+package lu.kbra.pclib.db.autobuild.sqlite.column.temporal;
+
+import java.lang.reflect.Type;
+import java.time.Year;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lu.kbra.pclib.db.autobuild.sqlite.encoding.integer.IntEncodingType;
+import lu.kbra.pclib.db.domain.column.type.ColumnType;
+import lu.kbra.pclib.db.domain.column.type.EncodingType;
+import lu.kbra.pclib.db.utils.registry.EncodingTypeRegistry;
+
+@Getter
+@RequiredArgsConstructor
+public class YearColumnType implements ColumnType<Year, Long> {
+
+	private final EncodingType<Long> encodingType = EncodingTypeRegistry.getFixedEncodingType(IntEncodingType.class, IntEncodingType::new);
+
+	@Override
+	public @NonNull Year decode(@NonNull Long value, Type type) {
+		return Year.of(value.intValue());
+	}
+
+	@Override
+	public @NonNull Long encode(@NonNull Year value) {
+		return (long) value.getValue();
+	}
+
+}
