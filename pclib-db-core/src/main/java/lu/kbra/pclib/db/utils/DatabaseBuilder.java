c8874d207b22cdfa2b9a31ab6c0283b8dfca7d74
UnKabaraQuiDev <u.kbra.lu@gmail.com>
2026-07-29 20:00:11 +0200

    DatabaseBuilder

---
+++ pclib-db-core/src/main/java/lu/kbra/pclib/db/utils/DatabaseBuilder.java
@@ -0,0 +1,12 @@
+package lu.kbra.pclib.db.utils;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+public class DatabaseBuilder {
+
+}
